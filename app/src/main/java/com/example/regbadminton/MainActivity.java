12a1ca87cace8351f5c6f1e4cd4598b045755fd5
Michael Levitski <levitski.michael@gmail.com>
2019-12-05 01:44:29 -0800

    remove intent

--- app/src/main/java/com/example/regbadminton/MainActivity.java
+++ app/src/main/java/com/example/regbadminton/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.regbadminton;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.browser.customtabs.CustomTabsIntent;
 
-import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
