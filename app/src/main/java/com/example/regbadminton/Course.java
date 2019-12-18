cceeca4ed70da6e2d0545d722743d8c42da0dd1f
regbadminton <49506031+regbadminton@users.noreply.github.com>
2019-12-17 22:09:38 -0800

    reorganize code

--- app/src/main/java/com/example/regbadminton/Course.java
+++ app/src/main/java/com/example/regbadminton/Course.java
@@ -11,7 +11,44 @@ public class Course {
     private String time;
     private String classID;
     private String location;
+    private String locationID;
+    private String comment;
 
+    public Course(JSONObject jsonCourse){
+        try {
+            date=jsonCourse.getString("date");
+            time=jsonCourse.getString("time");
+            classID=jsonCourse.getString("classID");
+            location=jsonCourse.getString("location");
+            locationID=jsonCourse.getString("locationID");
+            comment=jsonCourse.getString("comment");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public String getDate(String format) {
+        String date="";
+        try {
+            date=new SimpleDateFormat(format).format(new SimpleDateFormat("yyyy-MM-dd").parse(this.date));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getClassID() {
+        return classID;
+    }
+    
     public String getLocation() {
         return location;
     }
@@ -22,43 +59,5 @@ public class Course {
 
     public String getComment() {
         return comment;
-    }
-
-    private String locationID;
-    private String comment;
-
-    public Course(JSONObject jsonCourse){
-        try {
-            date=jsonCourse.getString("date");
-            time=jsonCourse.getString("time");
-            classID= jsonCourse.getString("classID");
-            location= jsonCourse.getString("location");
-            locationID= jsonCourse.getString("locationID");
-            comment= jsonCourse.getString("comment");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public String getDate(String format) {
-        String date="";
-        try {
-            date= new SimpleDateFormat(format).format(new SimpleDateFormat("yyyy-MM-dd").parse(this.date));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return date;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public String getClassID() {
-        return classID;
     }
 }
